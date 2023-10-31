ef9f7548064d3e213185dfe1a1e12c1bbeb58ddd
David A <dluna217@gmail.com>
2023-10-30 20:55:49 -0500

    change timezone

--- src/main/java/co/edu/iudigital/parqueadero/ParqueaderoApplication.java
+++ src/main/java/co/edu/iudigital/parqueadero/ParqueaderoApplication.java
@@ -3,10 +3,15 @@ package co.edu.iudigital.parqueadero;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+import java.util.logging.Logger;
+
 @SpringBootApplication
 public class ParqueaderoApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Bogota"));
+		Logger.getGlobal().info(TimeZone.getDefault().getDisplayName());
 		SpringApplication.run(ParqueaderoApplication.class, args);
 	}
 
